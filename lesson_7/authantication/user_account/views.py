74318572f281d934de89f9054ed9826b81013867
Allamyrat0906 <allamyrat.annayev.2001@gmail.com>
2024-02-20 22:52:00 +0500

    authantication

---
+++ lesson_7/authantication/user_account/views.py
@@ -0,0 +1,71 @@
+from multiprocessing import context
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
+
+
+def registerPage(request):
+    # return render(request, 'register.html')
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    else:
+
+        form = CreateUserForm()
+
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user_name = form.cleaned_data.get('username')
+                messages.success(
+                    request, 'Account was created for ' + user_name)
+                return redirect('login')
+
+        context = {'forms': form}
+        return render(request, 'register.html', context)
+
+
+def loginPage(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    else:
+
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            print(username, password)
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                # redirect is not for html file but for name of url or path
+                return redirect('home')
+
+            else:
+                messages.info(request, 'Incorrect Username/Password!!!')
+                return render(request, 'login.html')
+
+        return render(request, 'login.html')
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+# def userPage(request):
+#     return render(request, 'user.html')
